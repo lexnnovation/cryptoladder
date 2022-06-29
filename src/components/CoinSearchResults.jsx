26a64cd5a73059b5677fdafd67bc0280e97efa3e
Alexander Teye <alexander.teye29@gmail.com>
2022-06-29 21:27:58 +0000

    Final styling to crypto list

--- src/components/CoinSearchResults.jsx
+++ src/components/CoinSearchResults.jsx
@@ -8,8 +8,8 @@ import { Sparklines, SparklinesLine } from "react-sparklines";
 
 const CoinSearchResults = ({ coin }) => {
   return (
-    <tr className='h-[80px] border-b-[2px] last:border-b-0 overflow-hidden'>
-      <td>
+    <tr className='hover:bg-[#2b6cb0] hover:text-white ease-in duration-200 h-[80px] border-b-[2px] last:border-b-0 overflow-hidden'>
+      <td className='pl-3'>
         <AiOutlineStar />
       </td>
       <td>{coin.market_cap_rank}</td>
@@ -24,22 +24,26 @@ const CoinSearchResults = ({ coin }) => {
         </div>
       </td>
       <td>{coin.symbol.toUpperCase()}</td>
-      <td>{coin.current_price}</td>
+      <td>${coin.current_price.toLocaleString()}</td>
       <td>
         {coin.price_change_percentage_24h < 0 ? (
           <div className='text-red-600 pr-2 flex items-center justify-center'>
-            <p>{coin.price_change_percentage_24h}</p>
-            <AiOutlineArrowDown />
+            <p>{coin.price_change_percentage_24h.toFixed(3)}%</p>
+            <AiOutlineArrowDown size={10} />
           </div>
         ) : (
           <div className='text-green-600 pr-2 flex items-center justify-center'>
-            <p>{coin.price_change_percentage_24h}</p>
-            <AiOutlineArrowUp />
+            <p>{coin.price_change_percentage_24h.toFixed(3)}%</p>
+            <AiOutlineArrowUp size={10} />
           </div>
         )}
       </td>
-      <td className='w-[180px] hidden md:table-cell'>{coin.total_volume}</td>
-      <td className='w-[180px] hidden sm:table-cell'>{coin.market_cap}</td>
+      <td className='w-[180px] hidden md:table-cell'>
+        ${coin.total_volume.toLocaleString()}
+      </td>
+      <td className='w-[180px] hidden sm:table-cell'>
+        ${coin.market_cap.toLocaleString()}
+      </td>
       <td>
         <Sparklines data={coin.sparkline_in_7d.price}>
           <SparklinesLine color='red' />
