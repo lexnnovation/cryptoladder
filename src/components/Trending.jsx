7db1203fd66144d9e79b4a32748192f405b0b1c5
Alexander Teye <alexander.teye29@gmail.com>
2022-07-02 21:46:34 +0000

    styled the trending component

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -12,21 +12,29 @@ const Trending = () => {
   }, [url]);
 
   return (
-    <div className='rounded-container flex justify-between'>
-      <h1>Trending Coins</h1>
-      <div>
+    <div className='rounded-container my-12 py-4 text-primary'>
+      <h1 className='text-xl font-bold p-4'>Trending Coins</h1>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 mx-4'>
         {trending.map((trendingCoin) => (
-          <div>
-            <div>
-              <div>
-                <img src={trendingCoin.item.small} alt='/' />
-                <div>
-                  <p>{trendingCoin.item.name}</p>
+          <div
+            className='hover:bg-[#2b6cb0] hover:text-white  rounded-container flex justify-between p-4 hover:scale-105 ease-in-out duration-300'
+            key={trendingCoin.item.id}
+          >
+            <div className='flex items-center justify-between w-full'>
+              <div className='flex'>
+                <img
+                  className='rounded-full'
+                  src={trendingCoin.item.small}
+                  alt='/'
+                />
+                <div className='ml-4'>
+                  <p className='font-bold'>{trendingCoin.item.name}</p>
                   <p>{trendingCoin.item.symbol}</p>
                 </div>
               </div>
-              <div>
+              <div className='flex items-center'>
                 <img
+                  className='w-5 py-2 mr-1'
                   src='https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579'
                   alt='/'
                 />
