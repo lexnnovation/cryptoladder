963e1f80bb7cb1e145983a463c470f625c9865c7
Alexander Teye <alexander.teye29@gmail.com>
2022-09-18 12:35:53 +0000

    Created the Auth COntext for signin, sign up and sign out

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Account from "./routes/Account";
 import axios from "axios";
 import CoinPage from "./components/CoinPage";
 import Footer from "./components/Footer";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   const [coins, setCoins] = useState([]);
@@ -24,17 +25,19 @@ function App() {
 
   return (
     <ThemeProvider>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home coins={coins} />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/account' element={<Account />} />
-        <Route path='/coin/:coinId' element={<CoinPage />}>
-          <Route path=':coinId' />
-        </Route>
-      </Routes>
-      <Footer />
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home coins={coins} />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/account' element={<Account />} />
+          <Route path='/coin/:coinId' element={<CoinPage />}>
+            <Route path=':coinId' />
+          </Route>
+        </Routes>
+        <Footer />
+      </AuthContextProvider>
     </ThemeProvider>
   );
 }
