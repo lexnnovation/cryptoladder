49cd028d37ee313c589ec06195452a4230828169
Alexander Teye <alexander.teye29@gmail.com>
2022-09-18 10:59:15 +0000

    Added env and firebase files

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADsUeRLZ_zO5m8uxSFjQKLhWXMABwdfb0",
+  authDomain: "cryptolader.firebaseapp.com",
+  projectId: "cryptolader",
+  storageBucket: "cryptolader.appspot.com",
+  messagingSenderId: "941907248863",
+  appId: "1:941907248863:web:a2dffe556329b256878b6a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
